2f22f93a93645d3d7bbfa73fa94ee10ecb82379b
Jeongchanyeong <135792468020@naver.com>
2023-04-25 22:32:56 +0900

    변수 신문법 문제풀이

--- variable new grammar/apps.js
+++ variable new grammar/apps.js
@@ -1,30 +1,57 @@
-// 변수의 hoisting
-
-// 변수 동시에 여러개 만들기
-
-const 나이 = 20,
-  이름 = "정찬영",
-  성별 = "남자";
-
-// 전역 변수 = 모든 곳에서 쓸 수 있는 변수 (변수를 가장 바깥에 만들어 놓으면 어디서든 쓸 수 있음)
+// 문제 1
+함수();
 function 함수() {
-  const 이름 = "찬영"; // 지역변수 (지역 변수 만들 시 바깥보다 안 쪽 변수 값 할당)
-  console.log(나이); // console창에 20
-  console.log(이름);
+  let 안녕 = "Hello!";
+  console.log(안녕);
 }
-// 바깥에 있에 있는 변수값은 안에서 그대로 자유롭게 사용 가능함 (물려받음)
-// 전역 변수 만드는 법
-// 1. 바깥쪽에 변수 만들기
-// 2.  window 로 전역변수 만들기 (권장)
+// 콘솔창 출력 결과는? error
+// 해설 : let 안녕 은 hoisting이 될텐데 에러가 뜨는 이유는
+// let const : hoisting시 undefined가 자동으로 할당되지 않음
+// var : Hoisting시 undefined가 자동으로 할당
 
-// 연습문제
-if (true) {
-  let a = 1;
-  var b = 2;
-  if (true) {
-    let b = "3";
-  }
-  console.log(b);
+// 문제 2
+hello(); // Hello가 아니야
+var hello = function () {
+  console.log(안녕);
+  var 안녕 = "Hello!";
+};
+// 콘솔창 출력 결과는? error O 함수가 아닙니다 에러
+// 함수를 선언, 할당중
+// Hoisting은 변수의 선언부분만 되어 변수 선언부분만 맨 위로 끌어올려짐
+// 변수에 소괄호를 붙여봤자 아직 함수가 아님 (함수가 아닙니다 에러 뜸)
+
+// 문제 3
+let a = 1;
+var 함수 = function () {
+  a = 2;
+};
+console.log(a);
+
+// 콘솔창 출력 결과는? 1
+// 함수를 만들기만 하고 실행을 하지 않았음
+
+// 문제 4
+let b = 1;
+window.b = 3; // 전역변수로 b값을 또 만듬, let, const 재선언 x
+var c = 2;
+window.c = 4; // var은 전역변수 선언, 재선언 가능함으로 c 값을 대체
+console.log(b + c); // b=1, b=c
+// 콘솔창 출력 결과는? 5
+
+// 문제 5
+// var i = 5; // 반복문이 다 실행되고 전역변수로 남아있음
+
+for (var i = 1; i < 6; i++) {
+  setTimeout(function () {
+    console.log(i);
+  }, i * 1000);
 }
-// console.log(b) value = 2
-// let 변수의 범위는 중괄호 내부까지 있기 때문에 없다고 봐도 무방
+// Q. 위 코드는 왜 의도대로 동작하지 않는 것이죠? 설명, 해결 방법?
+
+// let으로 바꾸자 잘 작동함.. 이유는?
+// var은 범위가 함수, let은 범위가 {}
+
+// 1. 반복문 내 코드를 돌림,
+// 2. console.log 코드는 1초 뒤에 시작함
+// 3. 이미 반복문이 다 실행되고 전역변수로 var i = 5로 남아있음
+// 4. console 창에 5만 5번 반복
